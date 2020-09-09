1c1ab30f29ccffc37cdc332813dd73746bb63d3d
myslikam <ammyslik@gmail.com>
2020-09-09 20:17:56 +0300

    09.09.2020

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+      function coutntToTen() {
+        var count = 0;
+        while (count < 10) {
+        count++;
+        document.getElementById("theCount").innerHTML +=
+        count + "<br>";
+       }
+      }
+   
