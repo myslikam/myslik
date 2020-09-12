b08f6c1e1e4573550c001015234c3bd81f34b5f8
myslikam <ammyslik@gmail.com>
2020-09-12 09:25:41 +0300

    12.09.2020

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,29 @@
        }
       }
    
+/* var a = prompt("введите число 1");
+var b = prompt("введите число 2");
+var summa = Number(a) + Number(b);
+alert(summa); */
+
+/* var movie = "Крестный отец";
+/* function ShowGoodMovie () {
+  alert (movie + "- Хороший фильм");
+} */
+
+/* function ShowBadMovie () {
+  movie = "Фильма";
+  alert (movie + "- плохой фильм");
+} */ 
+
+/* var StrokaTyp = "Привет-пока";
+window.console.log (StrokaTyp.toLowerCase()); */
+
+/* var Sravnenie = Boolean (7);
+alert (Sravnenie); */
+var a = 5;
+if (a == 4) {
+  alert("Все верно");
+  } else {
+    alert("Брехня");
+  }
