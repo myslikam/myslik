23f3cc71c2d1a32a4eb4492aa67ae8ef514252e0
myslikam <ammyslik@gmail.com>
2020-09-14 21:15:32 +0300

    14.09.2020

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 
-      function coutntToTen() {
+    /*   function coutntToTen() {
         var count = 0;
         while (count < 10) {
         count++;
@@ -7,7 +7,7 @@
         count + "<br>";
        }
       }
-   
+    */
 /* var a = prompt("введите число 1");
 var b = prompt("введите число 2");
 var summa = Number(a) + Number(b);
@@ -28,9 +28,47 @@ window.console.log (StrokaTyp.toLowerCase()); */
 
 /* var Sravnenie = Boolean (7);
 alert (Sravnenie); */
-var a = 5;
+/* var a = 5;
 if (a == 4) {
   alert("Все верно");
   } else {
     alert("Брехня");
   }
+ */
+/* var MayMassiv = [];
+    MayMassiv[0]= "Один";
+      MayMassiv [0] [0] = "A";
+      MayMassiv [0] [1] = "Б";
+      MayMassiv [0] [2] = "В";
+    
+    MayMassiv [1]= "Два";
+    MayMassiv [1] [0] = "Г";
+    MayMassiv [1] [1] = "Д";
+
+    MayMassiv[2]= "Три"; 
+
+alert(MayMassiv[0][1]); */
+/* var arr = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
+ 
+alert(arr[1][2]); 
+ */
+/* var arr = [];
+arr [0] = [];
+arr [0] [0] = "1";
+arr [0] [1] = "2";
+arr [0] [2] = "3";
+arr [1] = [];
+arr [1] [0] = "4";
+arr [1] [2] = "5";
+arr [1] [3] = "6";
+arr [2] = [0];
+arr [2] [0] = "7";
+arr [2] [0] = "8";
+arr [2] [0] = "9";
+
+alert(arr[1][2]); 
+ */
